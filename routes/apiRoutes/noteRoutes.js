b9df471e738e7e2f5c166653b659df5aa7ca454d
BrianCKWang <brian.ck.wang@gmail.com>
2021-02-17 20:36:15 -0500

    set up initial server

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,37 @@
+const { filterByQuery, findById, createNewNote, validateNote } = require('../../lib/notes');
+const { notes } = require('../../db/db');
+const router = require('express').Router();
+
+router.get('/notes/', (req, res) => {
+  let results = notes;
+
+  if(req.query){
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+router.get('/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+
+  if (result) {
+    res.json(result);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+
+router.post('/notes', (req, res) => {
+  req.body.id = notes.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send('The Nnote is not properly formatted.');
+  } else {
+    const Nnote = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
+
+module.exports  = router;
