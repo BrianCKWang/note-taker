e5037afd418810b76c4ea161bd6864e194f5de28
BrianCKWang <brian.ck.wang@gmail.com>
2021-02-18 16:22:18 -0500

    fix the new note not being saved if it is the one from first launch

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -205,7 +205,7 @@ const renderNoteList = async (notes) => {
 };
 
 // Gets notes from the db and renders them to the sidebar
-const getAndRenderNotes = () => getNotes().then(renderNoteList);
+const getAndRenderNotes = () => getNotes().then(renderNoteList).then(handleNewNoteView);
 
 if (window.location.pathname === '/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
@@ -215,4 +215,3 @@ if (window.location.pathname === '/notes') {
 }
 
 getAndRenderNotes();
-// console.log(uuidv4());
